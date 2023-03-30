5739f316b1e449c625cb8e3a56bfe82771c8bc9e
Viniizada <127901612+Viniizada@users.noreply.github.com>
2023-03-30 20:40:50 -0300

    Add files via upload

---
+++ src/src/Cliente.java
@@ -0,0 +1,11 @@
+public class Cliente {
+    String nome;
+    int numero;
+    boolean tipo;
+
+    public Cliente(String nome, int numero, boolean tipo) {
+        this.nome = nome;
+        this.numero = numero;
+        this.tipo = tipo;
+    }
+}
